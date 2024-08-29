4819eda94f99837ac93b283e347ec26cf5548728
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-29 23:18:31 +0600

    create parameter_function.py

---
+++ Python/parameter_function.py
@@ -0,0 +1,9 @@
+def sum_2_number(first, second):
+    print(f"Sum of {first} + {second} = {first + second}")
+
+
+sum_2_number(10, 15)
+
+# Create a function for subtraction
+# Create a function for multiplication with 4 parameter
+# Similarly create function for Division, Concatenation (Concat)
