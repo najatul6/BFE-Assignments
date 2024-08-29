6d71d3abf6d0eaaf1f1b309b891b1ef172a72313
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-29 23:14:48 +0600

    complete combine function calling

---
+++ Python/combine_function.py
@@ -0,0 +1,18 @@
+def my_name_is():
+    print("My name is Najatul islam")
+
+
+def i_have_enrolled_course(course_name):
+    print(f"I have enrolled in a course named {course_name}")
+
+
+def i_have_learning(backend, frontend):
+    return f"Learning {backend}, {frontend}"
+
+
+my_name_is()
+i_have_enrolled_course("Python & Web")
+result = i_have_learning("Python", "HTML, CSS, JavaScript")
+print(result)
+
+# Update the code according your name, and different types of course listed in Bangla Fighter Website: https://banglafighter.com/crs
