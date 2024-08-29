e6e064ca82149112ba608868d00c3fc7da4f7857
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-29 23:18:06 +0600

    create loop.py

---
+++ Python/loop.py
@@ -0,0 +1,12 @@
+# For loop
+color_names = ["Red", "Green", "Blue", "Yellow"]
+for name in color_names:
+    print(name)
+
+
+# While loop
+start_count = 1
+end_count = 6
+while start_count <= end_count:
+    print(start_count)
+    start_count += 1
