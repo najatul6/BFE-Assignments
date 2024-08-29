d12d260ebd010649073701aeba2cb48a4aaf48e0
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-29 23:12:30 +0600

    example done

---
+++ Python/class_example.py
@@ -0,0 +1,9 @@
+class Mathmatics:
+
+    def sum_2_number(self, first, second):
+        return first + second
+
+
+math_object = Mathmatics()
+result = math_object.sum_2_number(10, 20)
+print(f"Result : {result}")
