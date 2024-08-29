8d2da3887adcb7748dc60a674da79966ca019ab8
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-29 23:16:51 +0600

    create if-else.py

---
+++ Python/if-else.py
@@ -0,0 +1,24 @@
+# If condition
+age = 25
+if age >= 18:
+    print("Hmm, You are adult.")
+
+
+# If else condition
+age = 16
+if age >= 18:
+    print("Hmm, You are adult.")
+else:
+    print("You are not adult.")
+
+
+# Nested If else condition
+age = 18
+if age < 2:
+    print("You are baby.")
+elif age < 11:
+    print("You are children.")
+elif age < 18:
+    print("You are adolescents.")
+else:
+    print("You are adult.")
