412ffd5c6ea9ca2a0310263f9f6ef2fbbbe2dd14
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-31 06:28:41 +0600

    create variable and show output

---
+++ Python/variable.py
@@ -0,0 +1,14 @@
+# Variable declare & assign value
+string_variable = "This is String Data"
+integer_variable = 100
+float_variable = 100.10
+boolean_variable = True
+
+print(f"String : {string_variable}")
+print(f"Integer: {integer_variable}")
+print(f"Float: {float_variable}")
+print(f"Boolean: {boolean_variable}")
+
+# Multiple variable Same value assignment
+weekend = public_holiday = eid_day = "Friday"
+print(weekend + " " + public_holiday + " " + eid_day)
