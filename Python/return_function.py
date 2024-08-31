b16456c6629e7fa1951c25830d02f107723940a3
Md Najatul Islam <mdnajatulislam.develop@gmail.com>
2024-08-31 06:26:15 +0600

    create return function

---
+++ Python/return_function.py
@@ -0,0 +1,8 @@
+def sum_2_number(first, second):
+    return first + second
+
+
+result = sum_2_number(10, 15)
+print(f"Summation result is : {result}")
+
+# Create a function for subtraction with return type
